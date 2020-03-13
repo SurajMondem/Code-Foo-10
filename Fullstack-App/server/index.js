d3a3689b41df0beb2d77f656eb3b0873f7951759
SurajMondem <surajmondem@gmail.com>
2020-03-13 18:07:42 -0400

    Connected MongoDB including DBtest file

--- Fullstack-App/server/index.js
+++ Fullstack-App/server/index.js
@@ -1,12 +1,19 @@
+require('dotenv').config();
 const express = require('express');
-const handle = require('./handlers')
+const db = require('./models');
+const handle = require('./handlers');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const app = express();
-const port = 3888;
+const PORT = process.env.PORT;
+
+app.use(cors());
+app.use(bodyParser.json());
 
 app.get('/', (request, response) => response.send("Hello world"));
 
 app.use(handle.notFound);
 app.use(handle.errorHandler);
 
-app.listen(port, console.log(`Server started on port ${port}`));
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
